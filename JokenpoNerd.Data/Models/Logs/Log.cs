6e82c6393a96d0a1c8fd75a6ca40f253c056a4d5
GleydsonMorais <morais_100@hotmail.com>
2021-07-25 14:09:03 -0300

    adicionando arquivos de log

---
+++ JokenpoNerd.Data/Models/Logs/Log.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace JokenpoNerd.Data.Models.Logs
+{
+    public class Log
+    {
+        public int Id { get; set; }
+        public string Opcao1 { get; set; }
+        public string Opcao2 { get; set; }
+        public string Mensagem { get; set; }
+        public DateTime DtInclusao { get; set; }
+    }
+}
---
+++ JokenpoNerd.Data/Repositories/Logs/LogRepository.cs
@@ -0,0 +1,37 @@
+﻿using JokenpoNerd.Data.Context;
+using JokenpoNerd.Data.Models.Logs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JokenpoNerd.Data.Repositories.Logs
+{
+    public interface ILogRepository
+    {
+        Task InsertLog(string opcao1, string opcao2, string mensagem);
+    }
+
+    public class LogRepository : ILogRepository
+    {
+        private readonly JokenpoNerdContext _dataContext;
+
+        public LogRepository(JokenpoNerdContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        public async Task InsertLog(string opcao1, string opcao2, string mensagem)
+        {
+            await _dataContext.AddAsync(
+            new Log()
+            {
+                Opcao1 = opcao1,
+                Opcao2 = opcao2,
+                Mensagem = mensagem,
+                DtInclusao = DateTime.Now
+            });
+            await _dataContext.SaveChangesAsync();
+        }
+    }
+}
